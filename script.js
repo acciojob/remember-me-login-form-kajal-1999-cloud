4234a8bccfde41ff69d3d64c3737af22129a60de
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-08-16 23:17:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 //your JS code here. If required.
+//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+  const loginForm = document.getElementById("loginForm");
+  const rememberMeCheckbox = document.getElementById("rememberMe");
+  const existingUserButton = document.getElementById("existing");
+
+  // Add event listener to the form submission
+  loginForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (rememberMeCheckbox.checked) {
+      saveUserDetails(username, password);
+    } else {
+      clearUserDetails();
+    }
+
+    alert("Logged in as " + username);
+  });
+
+  // Check if there are saved user details
+  const savedUsername = localStorage.getItem("username");
+  const savedPassword = localStorage.getItem("password");
+
+  if (savedUsername && savedPassword) {
+    const existingUserButton = document.createElement("button");
+    existingUserButton.id = "existing";
+    existingUserButton.textContent = "Login as existing user";
+    document.body.appendChild(existingUserButton);
+
+    existingUserButton.addEventListener("click", function() {
+      alert("Logged in as " + savedUsername);
+    });
+  }
+
+  // Utility functions
+  function saveUserDetails(username, password) {
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+  }
+
+  function clearUserDetails() {
+    localStorage.removeItem("username");
+    localStorage.removeItem("password");
+  }
+});
